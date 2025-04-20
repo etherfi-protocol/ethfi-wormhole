6316d3bce811cefd7e716f89e9156643bd4bfe23
jtfirek <firekjt@gmail.com>
2025-04-20 16:01:51 -0500

    adding scroll peer

--- test/baseCrossChainTest.t.sol
+++ test/baseCrossChainTest.t.sol
@@ -12,86 +12,102 @@ import {TrimmedAmountLib, TrimmedAmount, eq} from "@wormhole-foundation/native_t
 import {ERC20Upgradeable} from "openzeppelin-contracts-upgradeable/contracts/token/ERC20/ERC20Upgradeable.sol";
 import {EthfiL2Token} from "../src/token/EthfiL2Token.sol";    
 import {NttConstants} from "../utils/constants.sol";
+import {GnosisHelpers} from "../utils/GnosisHelpers.sol";
+
 // testing sends from all peers after adding base as a peer
-contract BaseDeploysSimSends is Test, NttConstants {
+contract ScrollDeploysSimSends is Test, NttConstants, GnosisHelpers {
 
     ERC20Upgradeable public mainnetEthfi;
     ERC20Upgradeable public arbEthfi;
     ERC20Upgradeable public baseEthfi;
+    ERC20Upgradeable public scrollEthfi;
 
     NttManager public mainnetNttManager;
     NttManager public arbNttManager;
     NttManager public baseNttManager;
+    NttManager public scrollNttManager;
 
     IWormholeTransceiver public mainnetTransceiver;
     IWormholeTransceiver public arbTransceiver;
     IWormholeTransceiver public baseTransceiver;
+    IWormholeTransceiver public scrollTransceiver;
+
+    address public scrollContractController;
+    address public baseContractController;
+    address public mainnetContractController;
+    address public arbContractController;
 
     function setUp() public {
         mainnetEthfi = ERC20Upgradeable(MAINNET_ETHFI);
         arbEthfi = ERC20Upgradeable(ARB_ETHFI);
         baseEthfi = ERC20Upgradeable(BASE_ETHFI);
+        scrollEthfi = ERC20Upgradeable(SCROLL_ETHFI);
 
         mainnetNttManager = NttManager(MAINNET_NTT_MANAGER);
         arbNttManager = NttManager(ARB_NTT_MANAGER);
         baseNttManager = NttManager(BASE_NTT_MANAGER);
+        scrollNttManager = NttManager(SCROLL_NTT_MANAGER);
 
         mainnetTransceiver = IWormholeTransceiver(MAINNET_TRANSCEIVER);
         arbTransceiver = IWormholeTransceiver(ARB_TRANSCEIVER);
         baseTransceiver = IWormholeTransceiver(BASE_TRANSCEIVER);
+        scrollTransceiver = IWormholeTransceiver(SCROLL_TRANSCEIVER);
+
+        mainnetContractController = MAINNET_CONTRACT_CONTROLLER;
+        arbContractController = ARB_CONTRACT_CONTROLLER;
+        baseContractController = BASE_CONTRACT_CONTROLLER;
+        scrollContractController = SCROLL_CONTRACT_CONTROLLER;
     }
 
     function testCrossChainSend() public {
-        vm.createSelectFork("https://mainnet.base.org");
+        vm.createSelectFork("https://scroll-mainnet.public.blastapi.io");
 
         simulateSend(ARB_WORMHOLE_ID);
         simulateSend(MAINNET_WORMHOLE_ID);
+        simulateSend(BASE_WORMHOLE_ID);
 
+        string memory BaseJson = _getGnosisHeader("8453");
+        BaseJson = string.concat(BaseJson, _getGnosisTransaction(address(baseTransceiver), abi.encodeWithSignature("setIsWormholeRelayingEnabled(uint16,bool)", SCROLL_WORMHOLE_ID, true), false));
+        BaseJson = string.concat(BaseJson, _getGnosisTransaction(address(baseTransceiver), abi.encodeWithSignature("setWormholePeer(uint16,bytes32)", SCROLL_WORMHOLE_ID, toWormholeFormat(SCROLL_TRANSCEIVER)), false));
+        BaseJson = string.concat(BaseJson, _getGnosisTransaction(address(baseTransceiver), abi.encodeWithSignature("setIsWormholeEvmChain(uint16,bool)", SCROLL_WORMHOLE_ID, true), false));
+        BaseJson = string.concat(BaseJson, _getGnosisTransaction(address(baseNttManager), abi.encodeWithSignature("setPeer(uint16,bytes32,uint8,uint256)", SCROLL_WORMHOLE_ID, toWormholeFormat(SCROLL_NTT_MANAGER), 18, 150000000000000000000000), true));
+
+        vm.writeFile("output/add-scroll-peer-base.json", BaseJson);
+
+        vm.createSelectFork("https://mainnet.base.org");
+        executeGnosisTransactionBundle("output/add-scroll-peer-base.json", baseContractController);
+        simulateSend(SCROLL_WORMHOLE_ID);
+
+        string memory mainnetJson = _getGnosisHeader("1");
+        mainnetJson = string.concat(mainnetJson, _getGnosisTransaction(address(mainnetTransceiver), abi.encodeWithSignature("setIsWormholeRelayingEnabled(uint16,bool)", SCROLL_WORMHOLE_ID, true), false));
+        mainnetJson = string.concat(mainnetJson, _getGnosisTransaction(address(mainnetTransceiver), abi.encodeWithSignature("setWormholePeer(uint16,bytes32)", SCROLL_WORMHOLE_ID, toWormholeFormat(SCROLL_TRANSCEIVER)), false));
+        mainnetJson = string.concat(mainnetJson, _getGnosisTransaction(address(mainnetTransceiver), abi.encodeWithSignature("setIsWormholeEvmChain(uint16,bool)", SCROLL_WORMHOLE_ID, true), false));
+        mainnetJson = string.concat(mainnetJson, _getGnosisTransaction(address(mainnetNttManager), abi.encodeWithSignature("setPeer(uint16,bytes32,uint8,uint256)", SCROLL_WORMHOLE_ID, toWormholeFormat(SCROLL_NTT_MANAGER), 18, 150000000000000000000000), true));
+
+        vm.writeFile("output/add-scroll-peer-mainnet.json", mainnetJson);
         vm.createSelectFork("https://eth-mainnet.public.blastapi.io");
+        executeGnosisTransactionBundle("output/add-scroll-peer-mainnet.json", mainnetContractController);
+        simulateSend(SCROLL_WORMHOLE_ID);
 
-        // transaction from gnosis to set base as a peer
-        vm.startPrank(0x2aCA71020De61bb532008049e1Bd41E451aE8AdC);
-        IWormholeTransceiver(0x3bf4AebcaD920447c5fdD6529239Ab3922ce2186).setIsWormholeRelayingEnabled(30, true);
-        IWormholeTransceiver(0x3bf4AebcaD920447c5fdD6529239Ab3922ce2186).setWormholePeer(30, toWormholeFormat(0x2153bEa70D96cd804aCbC89D82Ab36638fc1A5F4));
-        IWormholeTransceiver(0x3bf4AebcaD920447c5fdD6529239Ab3922ce2186).setIsWormholeEvmChain(30, true);
-        NttManager(0x344169Cc4abE9459e77bD99D13AA8589b55b6174).setPeer(30, toWormholeFormat(0xE87797A1aFb329216811dfA22C87380128CA17d8), 18, 150000000000000000000000);
-        vm.stopPrank();
 
-        simulateSend(BASE_WORMHOLE_ID);
+        string memory arbJson = _getGnosisHeader("42161");
+        arbJson = string.concat(arbJson, _getGnosisTransaction(address(arbTransceiver), abi.encodeWithSignature("setIsWormholeRelayingEnabled(uint16,bool)", SCROLL_WORMHOLE_ID, true), false));
+        arbJson = string.concat(arbJson, _getGnosisTransaction(address(arbTransceiver), abi.encodeWithSignature("setWormholePeer(uint16,bytes32)", SCROLL_WORMHOLE_ID, toWormholeFormat(SCROLL_TRANSCEIVER)), false));
+        arbJson = string.concat(arbJson, _getGnosisTransaction(address(arbTransceiver), abi.encodeWithSignature("setIsWormholeEvmChain(uint16,bool)", SCROLL_WORMHOLE_ID, true), false));
+        arbJson = string.concat(arbJson, _getGnosisTransaction(address(arbNttManager), abi.encodeWithSignature("setPeer(uint16,bytes32,uint8,uint256)", SCROLL_WORMHOLE_ID, toWormholeFormat(SCROLL_NTT_MANAGER), 18, 150000000000000000000000), true));
 
-         vm.createSelectFork("https://eth-mainnet.public.blastapi.io");
-
-        // transaction from gnosis to set base as a peer
-        vm.startPrank(0x2aCA71020De61bb532008049e1Bd41E451aE8AdC);
-        IWormholeTransceiver(0x3bf4AebcaD920447c5fdD6529239Ab3922ce2186).setIsWormholeRelayingEnabled(30, true);
-        IWormholeTransceiver(0x3bf4AebcaD920447c5fdD6529239Ab3922ce2186).setWormholePeer(30, toWormholeFormat(0x2153bEa70D96cd804aCbC89D82Ab36638fc1A5F4));
-        IWormholeTransceiver(0x3bf4AebcaD920447c5fdD6529239Ab3922ce2186).setIsWormholeEvmChain(30, true);
-        NttManager(0x344169Cc4abE9459e77bD99D13AA8589b55b6174).setPeer(30, toWormholeFormat(0xE87797A1aFb329216811dfA22C87380128CA17d8), 18, 150000000000000000000000);
-        vm.stopPrank();
-        
-        simulateSend(BASE_WORMHOLE_ID);
-
+        vm.writeFile("output/add-scroll-peer-arb.json", arbJson);
         vm.createSelectFork("https://arbitrum-one.public.blastapi.io");
-
-        // transaction from gnosis to set base as a peer
-        vm.startPrank(0x0c6ca434756EeDF928a55EBeAf0019364B279732);
-        IWormholeTransceiver(0x4386e36B96D437b0F1C04A35E572C10C6627d88a).setIsWormholeRelayingEnabled(30, true);
-        IWormholeTransceiver(0x4386e36B96D437b0F1C04A35E572C10C6627d88a).setWormholePeer(30, toWormholeFormat(0x2153bEa70D96cd804aCbC89D82Ab36638fc1A5F4));
-        IWormholeTransceiver(0x4386e36B96D437b0F1C04A35E572C10C6627d88a).setIsWormholeEvmChain(30, true);
-        NttManager(0x90A82462258F79780498151EF6f663f1D4BE4E3b).setPeer(30, toWormholeFormat(0xE87797A1aFb329216811dfA22C87380128CA17d8), 18, 150000000000000000000000);
-        vm.stopPrank();
-        
-        simulateSend(BASE_WORMHOLE_ID);
+        executeGnosisTransactionBundle("output/add-scroll-peer-arb.json", arbContractController);
+        simulateSend(SCROLL_WORMHOLE_ID);
        
     }
 
     function simulateSend(uint16 destinationPeer) public {
         address user = address(0x123);
-
         ERC20Upgradeable localEthfi = ERC20Upgradeable(address(0x0));
         NttManager localNttManager = NttManager(address(0x0));
 
-        vm.deal(user, 100_000 ether);
         if (block.chainid == 1)  {
             localEthfi = mainnetEthfi;
             localNttManager = mainnetNttManager;
@@ -100,24 +116,25 @@ contract BaseDeploysSimSends is Test, NttConstants {
             
         } else if (block.chainid == 42161) {
             vm.prank(ARB_NTT_MANAGER);
-            // the forge deal cheatcode doesn't work for our custom ERC-20 deployed to L2s in certain cases
             EthfiL2Token(ARB_ETHFI).mint(user, 50_000_000 ether);
 
             localEthfi = arbEthfi;
             localNttManager = arbNttManager;
-        } else {
-            // Minter hasn't been set yet
-            vm.prank(0xaFa61D537A1814DE82776BF600cb10Ff26342208);
-            EthfiL2Token(BASE_ETHFI).setMinter(BASE_NTT_MANAGER);
+        } else if (block.chainid == 8453) {
             vm.prank(BASE_NTT_MANAGER);
-            // the forge deal cheatcode doesn't work for our custom ERC-20 deployed to L2s in certain cases
             EthfiL2Token(BASE_ETHFI).mint(user, 50_000_000 ether);
 
             localEthfi = baseEthfi;
             localNttManager = baseNttManager;
+        } else {
+            vm.prank(SCROLL_NTT_MANAGER);
+            EthfiL2Token(SCROLL_ETHFI).mint(user, 50_000_000 ether);
+
+            localEthfi = scrollEthfi;
+            localNttManager = scrollNttManager;
         }
 
-        vm.startPrank(user);
+        startHoax(user);
         (,uint256 price) = localNttManager.quoteDeliveryPrice(destinationPeer, new bytes(1));
 
         // test a successful transfer
--- utils/constants.sol
+++ utils/constants.sol
@@ -13,16 +13,25 @@ contract NttConstants {
     address public constant MAINNET_NTT_MANAGER = 0x344169Cc4abE9459e77bD99D13AA8589b55b6174;
     address public constant MAINNET_TRANSCEIVER = 0x3bf4AebcaD920447c5fdD6529239Ab3922ce2186;
     address public constant MAINNET_ETHFI = 0xFe0c30065B384F05761f15d0CC899D4F9F9Cc0eB;
+    address public constant MAINNET_CONTRACT_CONTROLLER = 0x2aCA71020De61bb532008049e1Bd41E451aE8AdC;
 
     // arbitrum constants
     uint16 public constant ARB_WORMHOLE_ID = 23;
     address public constant ARB_NTT_MANAGER = 0x90A82462258F79780498151EF6f663f1D4BE4E3b;
     address public constant ARB_TRANSCEIVER = 0x4386e36B96D437b0F1C04A35E572C10C6627d88a;
     address public constant ARB_ETHFI = 0x7189fb5B6504bbfF6a852B13B7B82a3c118fDc27;
-
+    address public constant ARB_CONTRACT_CONTROLLER = 0x0c6ca434756EeDF928a55EBeAf0019364B279732;
     // base constants
     uint16 public constant BASE_WORMHOLE_ID = 30;
     address public constant BASE_NTT_MANAGER = 0xE87797A1aFb329216811dfA22C87380128CA17d8;
     address public constant BASE_TRANSCEIVER = 0x2153bEa70D96cd804aCbC89D82Ab36638fc1A5F4;
     address public constant BASE_ETHFI = 0x6C240DDA6b5c336DF09A4D011139beAAa1eA2Aa2;
+    address public constant BASE_CONTRACT_CONTROLLER = 0x7a00657a45420044bc526B90Ad667aFfaee0A868;
+
+    // scroll constants 
+    uint16 public constant SCROLL_WORMHOLE_ID = 34;
+    address public constant SCROLL_NTT_MANAGER = 0xEB1a3362aac9B3D96938BA03516F349D53829CF2;
+    address public constant SCROLL_TRANSCEIVER = 0x8bA09bD6Ed0d050F4969dE664F5c39a9ab85951B;
+    address public constant SCROLL_ETHFI = 0x056A5FA5da84ceb7f93d36e545C5905607D8bD81;
+    address public constant SCROLL_CONTRACT_CONTROLLER = 0x3cD08f51D0EA86ac93368DE31822117cd70CECA3;
 }
